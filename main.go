066581b22254dd6f0b0291a34e20028bc350a9dc
vidocco <jack.mk.thibaut@gmail.com>
2018-10-07 21:33:05 +0200

    feat: basic server running

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"github.com/listmera/frank/env"
+	"github.com/listmera/frank/middleware"
+	"github.com/listmera/frank/router"
+	"github.com/listmera/frank/utils"
+	"net/http"
 )
 
 func main() {
-//	r := router.NewRouter()
-//	handler := middleware.ApplyMiddleware(r)
-//	err := http.ListenAndServe(":3000", handler)
-//	if err != nil {
-//		panic(err)
-//	}
+	r := router.NewRouter()
+	handler := middleware.ApplyMiddleware(r)
+	err := http.ListenAndServe(env.GetOr("FRANK_PORT", ":3000"), handler)
+	utils.CheckErr(err)
 }
 
 func init() {
