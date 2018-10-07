733f9dd198a5e15777e2e0d42d1ee54f56ace59c
vidocco <jack.mk.thibaut@gmail.com>
2018-10-07 21:37:06 +0200

    style: added print of host and port

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"github.com/listmera/frank/middleware"
 	"github.com/listmera/frank/router"
 	"github.com/listmera/frank/utils"
+	"log"
 	"net/http"
 )
 
 func main() {
 	r := router.NewRouter()
 	handler := middleware.ApplyMiddleware(r)
+	log.Printf("Frank running in %s%s", env.GetOr("FRANK_HOST", "localhost"), env.GetOr("FRANK_PORT", ":3000"))
 	err := http.ListenAndServe(env.GetOr("FRANK_PORT", ":3000"), handler)
 	utils.CheckErr(err)
 }
