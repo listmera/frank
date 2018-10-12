138905b0df2e69fb7ddc9e18585a5452c1fac33d
vidocco <jack.mk.thibaut@gmail.com>
2018-10-12 17:03:04 +0200

    feat: created spotify user response struct

--- structs/spotify.structs.go
+++ structs/spotify.structs.go
@@ -18,3 +18,24 @@ type TokenRes struct {
 	AccessToken string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 }
+
+type SpotifyUser struct {
+	Birthdate string `json:"birthdate"`
+	Country string `json:"country"`
+	UserName string `json:"display_name"`
+	Email string `json:"email"`
+	Href string `json:"href"`
+	UserId string `json:"id"`
+	Images images `json:"images"`
+	Followers followers `json:"followers"`
+}
+
+type followers struct {
+	Total int `json:"total"`
+}
+
+type image struct {
+	Url string `json:"url"`
+}
+
+type images []image
